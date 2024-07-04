4a5c1f9211d89a2ab4bc1b235b027c5917165a85
Fabio2000 <frsilva2101@gmail.com>
2024-07-03 23:53:05 -0300

    modulando seção de apresentação do portifolio com fundo de estrelas  animado

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,10 +1,30 @@
 import react from 'react';
 import './home.css'
+import ReactTypingEffect from 'react-typing-effect';
+
 
 function Home() {
+
+    const title = '< Hellow World />'
+
     return (
-        <section className='background'>
-            <h1>Home</h1>
+        <section class="wrapper">
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            <div id="title">
+                <h1>{title}</h1>
+                <h2>Sou o Fabio Silva</h2>
+                <h3>
+                    <ReactTypingEffect
+                        text={['Analista de sistemas.', 'Desenvolvedor web.']}
+                        speed={80}           // Velocidade de digitação
+                        eraseSpeed={40}       // Velocidade de apagamento
+                        eraseDelay={2000}     // Atraso antes de apagar
+                        typingDelay={1000}    // Atraso antes de começar a digitar
+                    />
+                </h3>
+            </div>
         </section>
     )
 }
