3c9a45826f363143f3666e8d3d43f1f2a1ee17ef
Fabio2000 <frsilva2101@gmail.com>
2024-07-06 23:25:23 -0300

    Ajustando componentes para que as skills fique embaixo do historico profissional usando GRID do bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Historico from './components/historico/historico';
 import Home from './components/home/home';
 import NavBar from './components/navBar/navBar';
 import Resumo from './components/resumo/resumo';
+import Skills from './components/skills/skills';
 
 function App() {
   return (
@@ -14,11 +15,18 @@ function App() {
       <div>
         <Resumo />
       </div>
-      <div>
-      <Historico />
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-12 col-md-6 mb-4">
+            <Historico />
+          </div>
+          <div className="col-12 col-md-6">
+            <Skills />
+          </div>
+        </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default App;
