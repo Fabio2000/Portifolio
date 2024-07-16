4215eab85afddf6cea3de130d57628b05b00ba14
Fabio2000 <frsilva2101@gmail.com>
2024-07-15 23:38:32 -0300

    Melhorando slider infinito, e colocando função de pausa quando colocar mouse encima

--- src/components/carrossel/carrossel.js
+++ src/components/carrossel/carrossel.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './carrossel.css'
+import './carrossel.css';
+import { PiCertificateDuotone } from "react-icons/pi";
 import cert1 from '../../certificados/Linux.jpg';
 import cert2 from '../../certificados/MySQLPython.jpg';
 import cert3 from '../../certificados/OracleSQL.jpg';
@@ -8,48 +9,68 @@ import cert4 from '../../certificados/photoshop.jpg';
 import cert5 from '../../certificados/python.jpg';
 import cert6 from '../../certificados/ReactNative.jpg';
 
-
-
-const items = [
-    { src: cert1, alt: 'Certificado 1' },
-    { src: cert2, alt: 'Certificado 2' },
-    { src: cert3, alt: 'Certificado 3' },
-    { src: cert4, alt: 'Certificado 4' },
-    { src: cert5, alt: 'Certificado 5' },
-    { src: cert6, alt: 'Certificado 6' },
-]
-
 function Carrossel() {
+    const [isPaused, setIsPaused] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsPaused(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsPaused(false);
+    };
+
+    const handleClick = () => {
+        setIsPaused(!isPaused);
+    };
+
     return (
         <div className="" id="containerCarrossel">
             <article>
-                <h1>Certificações</h1>
+                <h1><PiCertificateDuotone size={70}/>Certificações</h1>
             </article>
             <section className="col-12 slider" id="carrossel">
-                <div className='slide-track'>
-                    <div className='slide'>
-                    <img src={cert1} class="img-fluid" alt="..."/>
+                <div className={`slide-track ${isPaused ? 'paused' : ''}`}>
+                    <div className='slide' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                        <img src={cert1} className="img-fluid" alt="..." />
                     </div>
-                    <div className='slide'>
-                    <img src={cert2} class="img-fluid" alt="..."/>
+                    <div className='slide' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                        <img src={cert2} className="img-fluid" alt="..." />
                     </div>
-                    <div className='slide'>
-                    <img src={cert3} class="img-fluid" alt="..."/>
+                    <div className='slide' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                        <img src={cert3} className="img-fluid" alt="..." />
                     </div>
-                    <div className='slide'>
-                    <img src={cert4} class="img-fluid" alt="..."/>
+                    <div className='slide' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                        <img src={cert4} className="img-fluid" alt="..." />
                     </div>
-                    <div className='slide'>
-                    <img src={cert5} class="img-fluid" alt="..."/>
+                    <div className='slide' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                        <img src={cert5} className="img-fluid" alt="..." />
                     </div>
-                    <div className='slide'>
-                    <img src={cert6} class="img-fluid" alt="..."/>
+                    <div className='slide' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                        <img src={cert6} className="img-fluid" alt="..." />
+                    </div>
+                    <div className='slide' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                        <img src={cert1} className="img-fluid" alt="..." />
+                    </div>
+                    <div className='slide' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                        <img src={cert2} className="img-fluid" alt="..." />
+                    </div>
+                    <div className='slide' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                        <img src={cert3} className="img-fluid" alt="..." />
+                    </div>
+                    <div className='slide' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                        <img src={cert4} className="img-fluid" alt="..." />
+                    </div>
+                    <div className='slide' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                        <img src={cert5} className="img-fluid" alt="..." />
+                    </div>
+                    <div className='slide' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                        <img src={cert6} className="img-fluid" alt="..." />
                     </div>
                 </div>
-            </section >
-        </div >
+            </section>
+        </div>
     )
-
 }
 
-export default Carrossel
+export default Carrossel;
