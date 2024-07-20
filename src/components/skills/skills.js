0aef7ee662bd58e557b6c5923a22a330c5e7a573
Fabio2000 <frsilva2101@gmail.com>
2024-07-19 23:01:41 -0300

    Tentando melhorar as skills V2

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -7,7 +7,20 @@ import { RiJavascriptFill } from "react-icons/ri";
 import { IoLogoPython } from "react-icons/io5";
 import { SiMysql, SiNextdotjs, SiBootstrap, SiMongodb } from "react-icons/si";
 import { FaNode, FaReact } from "react-icons/fa";
+import { Button } from 'reactstrap';
 
+
+
+const skills = [
+    { name: 'CSS', color: '#264de4', icon: 'https://path/to/css-icon.png' },
+    { name: 'HTML5', color: '#e34c26', icon: 'https://path/to/html-icon.png' },
+    { name: 'npm', color: '#cb3837', icon: 'https://path/to/npm-icon.png' },
+    { name: 'React', color: '#61dbfb', icon: 'https://path/to/react-icon.png' },
+    { name: 'TypeScript', color: '#007acc', icon: 'https://path/to/typescript-icon.png' },
+    { name: 'git', color: '#f05032', icon: 'https://path/to/git-icon.png' },
+    { name: 'Redux', color: '#764abc', icon: 'https://path/to/redux-icon.png' },
+    { name: 'JavaScript', color: '#f0db4f', icon: 'https://path/to/javascript-icon.png' }
+  ];
 
 
 
@@ -25,8 +38,19 @@ function Skills() {
             </div>
             <div className="container" id="SkillsCards">
                 <div className="row">
-                    <div className="col-md-4">
-                    </div>
+                {skills.map((skill, index) => (
+          <Button
+            key={index}
+            style={{
+              backgroundColor: skill.color,
+              borderColor: skill.color,
+              margin: '10px'
+            }}
+          >
+            <img src={skill.icon} alt={skill.name} style={{ width: '24px', marginRight: '8px' }} />
+            {skill.name}
+          </Button>
+        ))}
                 </div>
             </div>
 
