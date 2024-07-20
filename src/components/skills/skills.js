cde90490f1b643245ff14434773242a9ebe5397d
Fabio2000 <frsilva2101@gmail.com>
2024-07-19 23:21:17 -0300

    Tentando melhorar as skills V2

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -7,22 +7,9 @@ import { RiJavascriptFill } from "react-icons/ri";
 import { IoLogoPython } from "react-icons/io5";
 import { SiMysql, SiNextdotjs, SiBootstrap, SiMongodb } from "react-icons/si";
 import { FaNode, FaReact } from "react-icons/fa";
-import { Button } from 'reactstrap';
+import {Button} from 'reactstrap';
 
-
-
-const skills = [
-    { name: 'CSS', color: '#264de4', icon: 'https://path/to/css-icon.png' },
-    { name: 'HTML5', color: '#e34c26', icon: 'https://path/to/html-icon.png' },
-    { name: 'npm', color: '#cb3837', icon: 'https://path/to/npm-icon.png' },
-    { name: 'React', color: '#61dbfb', icon: 'https://path/to/react-icon.png' },
-    { name: 'TypeScript', color: '#007acc', icon: 'https://path/to/typescript-icon.png' },
-    { name: 'git', color: '#f05032', icon: 'https://path/to/git-icon.png' },
-    { name: 'Redux', color: '#764abc', icon: 'https://path/to/redux-icon.png' },
-    { name: 'JavaScript', color: '#f0db4f', icon: 'https://path/to/javascript-icon.png' }
-  ];
-
-
+const seila = [{color: '#264de4'}]
 
 
 function Skills() {
@@ -36,21 +23,17 @@ function Skills() {
                     Habilidades
                 </h1>
             </div>
-            <div className="container" id="SkillsCards">
+            <div className="container" id="SkillsButtons">
                 <div className="row">
-                {skills.map((skill, index) => (
-          <Button
-            key={index}
-            style={{
-              backgroundColor: skill.color,
-              borderColor: skill.color,
-              margin: '10px'
-            }}
-          >
-            <img src={skill.icon} alt={skill.name} style={{ width: '24px', marginRight: '8px' }} />
-            {skill.name}
-          </Button>
-        ))}
+                    <div className="col-md-4">
+                    <Button
+                    color="primary"
+                    disabled
+                  >
+                    <FaHtml5 size={40} color="red"/>
+                    <strong>HTML 5</strong>
+                  </Button>
+                    </div>
                 </div>
             </div>
 
