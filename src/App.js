eacbea4e61f2a4994c2e3b3607f949bd394eb9fa
Fabio2000 <frsilva2101@gmail.com>
2024-07-25 00:50:24 -0300

    Adicionando animação fade-up nos meus componentes utilizando a biblioteca AOS

--- src/App.js
+++ src/App.js
@@ -6,31 +6,42 @@ import NavBar from './components/navBar/navBar';
 import Resumo from './components/resumo/resumo';
 import Skills from './components/skills/skills';
 import Project from './components/projetos/project';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Duração da animação em milissegundos
+      offset: 200, // Offset da animação em pixels
+      once: false, // Se verdadeiro, a animação ocorre apenas uma vez
+    });
+  }, []);
+
   return (
     <section>
       <NavBar />
-      <div id='home'>
+      <div id='home' data-aos="fade-up">
         <Home />
       </div>
-      <div id='resumo'>
+      <div id='resumo' data-aos="fade-up">
         <Resumo />
       </div>
       <div className='container'>
         <div className='row'>
-          <div className='col-sm-12 col-md-6' id='Histrico'>
+          <div className='col-sm-12 col-md-6' id='Historico' data-aos="fade-up">
             <Historico />
           </div>
-          <div className='col-sm-12 col-md-6' id='Skills'>
+          <div className='col-sm-12 col-md-6' id='Skills' data-aos="fade-up">
             <Skills />
           </div>
         </div>
       </div>
-      <div className='col-lg-12' id='Carrossel'>
+      <div className='col-lg-12' id='Carrossel' data-aos="fade-up">
         <Carrossel />
       </div>
-      <div id='projects'>
+      <div id='Projects' data-aos="fade-up">
         <Project />
       </div>
     </section>
